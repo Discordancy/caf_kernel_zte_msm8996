be481c5572fe8e4a84346dd67c394adbb469c1d8
Sven Eckelmann <sven@narfation.org>
2013-11-09 19:25:57 +0100

    HID: sony: Add force feedback support for Dualshock3 USB
    Sony Dualshock 3 controllers have two motors which can be used to provide
    simple force feedback rumble effects. The right motor is can be used to create
    a weak rumble effect but does not allow to set the force. The left motor is
    used to create a strong rumble effect with adjustable intensity.
    
    The state of both motors can be changed using HID_OUTPUT_REPORT packets and
    have no timing information. FF memless is used to keep track of the timing and
    the sony driver just generates the necessary URBs.
    
    Signed-off-by: Sven Eckelmann <sven@narfation.org>
    Signed-off-by: Jiri Kosina <jkosina@suse.cz>

--- drivers/hid/hid-sony.c
+++ drivers/hid/hid-sony.c
@@ -1868,6 +1868,54 @@ static inline void sony_cancel_work_sync(struct sony_sc *sc)
 		cancel_work_sync(&sc->state_worker);
 }
 
+#ifdef CONFIG_SONY_FF
+static int sony_play_effect(struct input_dev *dev, void *data,
+			    struct ff_effect *effect)
+{
+	unsigned char buf[] = {
+		0x01,
+		0x00, 0xff, 0x00, 0xff, 0x00,
+		0x00, 0x00, 0x00, 0x00, 0x03,
+		0xff, 0x27, 0x10, 0x00, 0x32,
+		0xff, 0x27, 0x10, 0x00, 0x32,
+		0xff, 0x27, 0x10, 0x00, 0x32,
+		0xff, 0x27, 0x10, 0x00, 0x32,
+		0x00, 0x00, 0x00, 0x00, 0x00
+	};
+	__u8 left;
+	__u8 right;
+	struct hid_device *hid = input_get_drvdata(dev);
+
+	if (effect->type != FF_RUMBLE)
+		return 0;
+
+	left = effect->u.rumble.strong_magnitude / 256;
+	right = effect->u.rumble.weak_magnitude ? 1 : 0;
+
+	buf[3] = right;
+	buf[5] = left;
+
+	return hid->hid_output_raw_report(hid, buf, sizeof(buf),
+					  HID_OUTPUT_REPORT);
+}
+
+static int sony_init_ff(struct hid_device *hdev)
+{
+	struct hid_input *hidinput = list_entry(hdev->inputs.next,
+						struct hid_input, list);
+	struct input_dev *input_dev = hidinput->input;
+
+	input_set_capability(input_dev, EV_FF, FF_RUMBLE);
+	return input_ff_create_memless(input_dev, NULL, sony_play_effect);
+}
+
+#else
+static int sony_init_ff(struct hid_device *hdev)
+{
+	return 0;
+}
+#endif
+
 static int sony_probe(struct hid_device *hdev, const struct hid_device_id *id)
 {
 	int ret;
@@ -1953,6 +2001,7 @@ static int sony_probe(struct hid_device *hdev, const struct hid_device_id *id)
 	if (ret < 0)
 		goto err_stop;
 
+<<<<<<< HEAD
 	ret = sony_check_add(sc);
 	if (ret < 0)
 		goto err_stop;
@@ -1982,6 +2031,12 @@ static int sony_probe(struct hid_device *hdev, const struct hid_device_id *id)
 			goto err_close;
 	}
 
+=======
+	ret = sony_init_ff(hdev);
+	if (ret < 0)
+		goto err_stop;
+
+>>>>>>> a08c22c0df0a... HID: sony: Add force feedback support for Dualshock3 USB
 	return 0;
 err_close:
 	hid_hw_close(hdev);
